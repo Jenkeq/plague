b6bfa8593c198364ffca72906fb96fc98eb81b9e
Jenkeq <189082984@qq.com>
2022-04-22 11:12:30 +0800

    新增订单模块

---
+++ plague-order/order-boot/src/main/java/com/gover/plague/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gover.plague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
