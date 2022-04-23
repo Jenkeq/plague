e19fd245885eeee0d84e70ad19e0e4502f596010
Jenkeq <189082984@qq.com>
2022-04-23 20:41:53 +0800

    集成spring cloud alibaba nacos discovery

--- plague-order/order-persistence/src/main/java/com/gover/plague/mapper/OrderPlaceMapper.java
+++ plague-order/order-persistence/src/main/java/com/gover/plague/mapper/OrderPlaceMapper.java
@@ -1,5 +1,6 @@
 package com.gover.plague.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gover.plague.trans.dto.OrderDO;
 import com.gover.plague.value.OrderId;
 import org.apache.ibatis.annotations.Mapper;
@@ -8,7 +9,7 @@ import org.apache.ibatis.annotations.Param;
 import java.util.Map;
 
 @Mapper
-public interface OrderPlaceMapper {
+public interface OrderPlaceMapper extends BaseMapper<OrderDO> {
 
     OrderDO findOrder(@Param("orderId") OrderId id);
 }
